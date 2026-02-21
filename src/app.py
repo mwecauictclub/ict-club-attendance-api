133fe6242058856dfcebc1780dd41d82d2bba47b
Monaty255 <mabelemariboto@gmail.com>
2026-02-21 11:12:23 +0300

    Update app.py
    Remove unnecessary code on main.py

--- src/app.py
+++ src/app.py
@@ -31,15 +31,4 @@ register_error_handlers(app)
 
 if __name__ == '__main__':
     # Run the application
-    app.run()
-
-# A very simple Flask Hello World app for you to get started with...
-
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def hello_world():
-#     return 'Hello from Flask!'
-
+    app.run(
